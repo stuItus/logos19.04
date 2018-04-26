e1a5fb00de1353fa0231fa270b7b871d9c007fc0
stuItus <popadynrs@gmail.com>
2018-04-26 16:19:45 +0300

    mvc

---
+++ SpringBootSecurityApp/src/main/java/ua/logos/MvcConfig.java
@@ -0,0 +1,10 @@
+package ua.logos;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig {
+
+}
